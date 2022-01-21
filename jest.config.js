76487bc938d5864af539be7337f14974fba2bdaf
albertoflorence <alberto.sabatier@gmail.com>
2022-01-21 19:44:31 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
