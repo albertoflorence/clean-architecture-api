f6aead9a15b13ba7af18bc52d01a35ca4635cb86
albertoflorence <alberto.sabatier@gmail.com>
2022-01-22 02:42:03 -0300

    refactor: implements an interface to SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missingParamError'
 import { badRequest } from '../helpers/httpHelpper'
+import { Controller } from '../protocols/controllers'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handler = (httpRequest: HttpRequest): HttpResponse => {
     const missingParam = ['name', 'email', 'password', 'passwordConfirm'].find(
       param => !httpRequest.body[param]
