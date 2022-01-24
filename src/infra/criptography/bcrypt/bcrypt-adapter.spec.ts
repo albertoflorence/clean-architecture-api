dc3fd61435206e11c38bd40f9b1faef47ef156e8
albertoflorence <alberto.sabatier@gmail.com>
2022-01-24 15:42:51 -0300

    test: ensure BcryptAdapter return a rashed value on success

--- src/infra/criptography/bcrypt/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt/bcrypt-adapter.spec.ts
@@ -10,11 +10,21 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 
+jest.mock('bcrypt', () => ({
+  hash: async () => await Promise.resolve('hashed_any_value')
+}))
+
 describe('BcryptAdapter Adapter', () => {
   it('Should call bcrypt with correct values', async () => {
     const { sut } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+
+  it('Should return a hashed on success', async () => {
+    const { sut } = makeSut()
+    const hashedValue = await sut.encrypt('any_value')
+    expect(hashedValue).toBe('hashed_any_value')
   })
 })
