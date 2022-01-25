eded71425d00ff0a43a855022dd20bcc4329463d
albertoflorence <alberto.sabatier@gmail.com>
2022-01-24 22:27:43 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/criptography/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,25 @@
+import { mongoDbHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account MongoDb Repository', () => {
+  beforeAll(async () => {
+    await mongoDbHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await mongoDbHelper.disconnect()
+  })
+
+  it('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
