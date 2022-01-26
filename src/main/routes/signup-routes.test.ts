1b0ae1c693cbb8857163e68ca9036e8ccc784c12
albertoflorence <alberto.sabatier@gmail.com>
2022-01-25 22:40:59 -0300

    test: ensure SignUpController handles request to /api/signup and return the created account

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import { MongoDbHelper } from '../../infra/criptography/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoDbHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoDbHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await MongoDbHelper.getCollection('accounts').deleteMany({})
+  })
+
+  it('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirm: 'valid_password'
+      })
+      .expect(200)
+  })
+})
