e081e4111e8dcc70fc471454d4a101786afd317f
albertoflorence <alberto.sabatier@gmail.com>
2022-01-25 21:36:44 -0300

    refactor: ensure server load after mongodb

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,12 @@
-import app from './config/app'
+import { MongoDbHelper } from '../infra/criptography/db/mongodb/helpers/mongo-helper'
 
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+import env from './config/env'
+
+void MongoDbHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () =>
+      console.log(`Server running at http://localhost:${env.port}`)
+    )
+  })
+  .catch(console.error)
