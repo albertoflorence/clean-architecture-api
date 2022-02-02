27f823aaee5dd9832e28556741d0b2e9fafd67c4
albertoflorence <alberto.sabatier@gmail.com>
2022-02-02 20:00:53 -0300

    test: ensure LogErrorMongoRepository create an error log on success

---
+++ src/infra/cryptography/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,31 @@
+import { Collection } from 'mongodb'
+import { LogErrorMongoRepository } from '.'
+import { MongoDbHelper } from '../helpers/mongo-helper'
+
+describe('Account MongoDb Repository', () => {
+  let collection: Collection
+  beforeAll(async () => {
+    await MongoDbHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoDbHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    collection = MongoDbHelper.getCollection('errors')
+    await collection.deleteMany({})
+  })
+
+  const makeSut = (): LogErrorMongoRepository => {
+    return new LogErrorMongoRepository()
+  }
+
+  it('Should return an log on success', async () => {
+    const sut = makeSut()
+    const stack = 'Error Message Stack'
+    await sut.saveLog(stack)
+    const count = await collection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
