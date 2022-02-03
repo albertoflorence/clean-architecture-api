8f597c14eaa39393a99f1cfc684ec57749503d9c
albertoflorence <alberto.sabatier@gmail.com>
2022-02-02 22:16:45 -0300

    test: ensure LogController return 400 if an invalid email is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,14 +1,23 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers'
+import { EmailValidator } from '../signup/signup-protocols'
 import { LoginController } from './login'
 
 interface SutTypes {
   sut: LoginController
+  emailValidatorStub: EmailValidator
+}
+
+class EmailValidatorStub implements EmailValidator {
+  isValid(email: string): boolean {
+    return true
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController()
-  return { sut }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new LoginController(emailValidatorStub)
+  return { sut, emailValidatorStub }
 }
 
 describe('Login Controller', () => {
@@ -23,14 +32,31 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handler(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
+
   it('Should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
-        email: 'valid_email'
+        email: 'valid_mail@mail.com'
       }
     }
     const httpResponse = await sut.handler(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  it('Should return 400 if a invalid email is provided', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest
+      .spyOn(emailValidatorStub, 'isValid')
+      .mockImplementationOnce(() => false)
+
+    const httpRequest = {
+      body: {
+        email: 'invalid_mail@mail.com',
+        password: 'valid_password'
+      }
+    }
+    const httpResponse = await sut.handler(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 })
