dacb734b29b9948b14e5be9dd4ab81738290da5c
albertoflorence <alberto.sabatier@gmail.com>
2022-02-04 07:36:48 -0300

    refactor: move data layer interfaces to folders

--- src/data/usecases/add-account/protocols.ts
+++ src/data/usecases/add-account/protocols.ts
@@ -1,5 +1,5 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
-export * from '../../protocols/encrypter'
+export * from '../../protocols/cryptography/encrypter'
 export * from './db-add-account'
-export * from '../../../data/protocols/add-account-repository'
+export * from '../../protocols/db/add-account-repository'
