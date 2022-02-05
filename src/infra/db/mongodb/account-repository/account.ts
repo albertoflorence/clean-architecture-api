a5200ab62e40e1491d3be9d23d899b3055366677
albertoflorence <alberto.sabatier@gmail.com>
2022-02-05 17:48:35 -0300

    feat: ensure AccountMongoRepository retuns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,12 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoDbHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   add = async (account: AddAccountModel): Promise<AccountModel> => {
     const result = await MongoDbHelper.getCollection('accounts').insertOne(
       account
@@ -15,5 +18,30 @@ export class AccountMongoRepository implements AddAccountRepository {
       email: account.email,
       password: account.password
     }
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel | null> {
+    const result = await MongoDbHelper.getCollection('accounts').findOne(
+      {
+        email
+      },
+      {
+        projection: {
+          _id: 1,
+          name: 1,
+          password: 1,
+          email: 1
+        }
+      }
+    )
+    if (result) {
+      return {
+        id: result._id.toString(),
+        name: result.name,
+        email: result.email,
+        password: result.password
+      }
+    }
+    return null
   }
 }
