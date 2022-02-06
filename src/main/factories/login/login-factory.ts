1735a082b8dc061a65e977493004430a4fffca6e
albertoflorence <alberto.sabatier@gmail.com>
2022-02-05 23:18:34 -0300

    feat: add login factory

---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,31 @@
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter/jwt.adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { LogErrorMongoRepository } from '../../../infra/db/mongodb/log'
+import {
+  Controller,
+  LoginController
+} from '../../../presentation/controllers/login/login-controller-protocols'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import { makeLoginValidation } from './login-validation-factory'
+import env from '../../config/env'
+
+export const makeLoginController = (): Controller => {
+  const authentication = new DbAuthentication(
+    new AccountMongoRepository(),
+    new BcryptAdapter(env.bcryptSalt),
+    new JwtAdapter(env.jwtSecret),
+    new AccountMongoRepository()
+  )
+
+  const loginController = new LoginController(
+    makeLoginValidation(),
+    authentication
+  )
+
+  return new LogControllerDecorator(
+    loginController,
+    new LogErrorMongoRepository()
+  )
+}
