0848179b41ce958ce91930958c1c7f0fad28c043
albertoflorence <alberto.sabatier@gmail.com>
2022-02-06 22:54:49 -0300

    chore: logging env variables

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,8 +3,10 @@ import env from './config/env'
 void MongoDbHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () =>
+    app.listen(env.port, () => {
       console.log(`Server running at http://localhost:${env.port}`)
-    )
+      console.log(process.env)
+      console.log(process.env.MONGO_URL)
+    })
   })
   .catch(console.error)
