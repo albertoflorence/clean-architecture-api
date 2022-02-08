cbc094041ed286f3dac53b6c04ea6e31648699e0
albertoflorence <alberto.sabatier@gmail.com>
2022-02-07 23:52:18 -0300

    feat: ensure SignUpValidationFactory verify if email already exist

--- src/main/factories/controllers/signup/signup-validation-factory.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.ts
@@ -1,18 +1,22 @@
 import { EmailValidatorAdapter } from '../../../../infra/validators'
+import { UniqueFieldMongoAdapter } from '../../../../infra/validators/unique-field-mongo-adapter'
 import {
   CompareFieldsValidation,
   EmailValidation,
   RequireFieldValidation,
   Validation,
-  ValidationComposite
+  UniqueFieldValidation,
+  ValidationAsync,
+  ValidationCompositeAsync
 } from '../../../../validation'
 
-export const makeSignUpValidation = (): Validation => {
-  const validations: Validation[] = [
+export const makeSignUpValidation = (): ValidationAsync => {
+  const validations: Array<Validation | ValidationAsync> = [
     new RequireFieldValidation('name', 'email', 'password', 'passwordConfirm'),
     new CompareFieldsValidation('password', 'passwordConfirm'),
-    new EmailValidation('email', new EmailValidatorAdapter())
+    new EmailValidation('email', new EmailValidatorAdapter()),
+    new UniqueFieldValidation('email', new UniqueFieldMongoAdapter('accounts'))
   ]
 
-  return new ValidationComposite(validations)
+  return new ValidationCompositeAsync(validations)
 }
