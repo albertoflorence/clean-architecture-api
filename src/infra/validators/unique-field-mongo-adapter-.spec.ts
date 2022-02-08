96496a3b2d412fffda2af1a14fedc7a62d0f9204
albertoflorence <alberto.sabatier@gmail.com>
2022-02-07 23:50:15 -0300

    test: ensure UniqueFieldMongoAdapter verify if value already exist on mongodb

---
+++ src/infra/validators/unique-field-mongo-adapter-.spec.ts
@@ -0,0 +1,41 @@
+import { Collection } from 'mongodb'
+import { MongoDbHelper } from '../db/mongodb/helpers/mongo-helper'
+import { UniqueFieldMongoAdapter } from './unique-field-mongo-adapter'
+
+describe('Unique Field Mongo Adapter', () => {
+  let collection: Collection
+  beforeAll(async () => {
+    await MongoDbHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoDbHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    collection = MongoDbHelper.getCollection('any_collection')
+    await collection.deleteMany({})
+  })
+
+  const makeSut = (): UniqueFieldMongoAdapter => {
+    return new UniqueFieldMongoAdapter('any_collection')
+  }
+
+  it('Should return false if the value already exist', async () => {
+    const sut = makeSut()
+    const input = { field: 'unique_value' }
+
+    await collection.insertOne(input)
+    const isUnique = await sut.isUnique(input)
+    expect(isUnique).toBe(false)
+  })
+
+  it('Should return true if value not exist', async () => {
+    const sut = makeSut()
+    const input = { field: 'any_value' }
+
+    const isUnique = await sut.isUnique(input)
+
+    expect(isUnique).toBe(true)
+  })
+})
