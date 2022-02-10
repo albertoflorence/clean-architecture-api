79f27922b2258aab768af921765f79cbcfadd04e
albertoflorence <alberto.sabatier@gmail.com>
2022-02-09 23:09:20 -0300

    test: ensure SurveyRoutes only grant access to admin role accounts

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,11 +1,46 @@
+import jwt from 'jsonwebtoken'
+import env from '../config/env'
 import request from 'supertest'
 import {
   MongoDbHelper,
   Collection
 } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { AddSurveyModel } from '../../domain/usecases/add-survey'
 
 let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeFakeSurvey = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'http://image-name.com'
+    },
+    { answer: 'another_answer' }
+  ]
+})
+
+const makeUserAccountWithRole = async (
+  role: string
+): Promise<{ accessToken: string }> => {
+  const res = await accountCollection.insertOne({
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    password: 'valid_password',
+    accessToken: 'any_token',
+    role
+  })
+  const id = res.insertedId.toHexString()
+  const accessToken = jwt.sign(id, env.jwtSecret)
+  await accountCollection.updateOne(
+    { _id: res.insertedId },
+    { $set: { accessToken } }
+  )
+
+  return { accessToken }
+}
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -19,23 +54,32 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = MongoDbHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = MongoDbHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
     it('Should return 403 on survey without accessToken', async () => {
+      await request(app).post('/api/surveys').send(makeFakeSurvey()).expect(403)
+    })
+
+    it('Should return 403 on survey if role is not admin', async () => {
+      const { accessToken } = await makeUserAccountWithRole('not_admin')
       await request(app)
         .post('/api/surveys')
-        .send({
-          question: 'any_question',
-          answers: [
-            {
-              image: 'any_image',
-              answer: 'http://image-name.com'
-            },
-            { answer: 'another_answer' }
-          ]
-        })
+        .set('x-access-token', accessToken)
+        .send(makeFakeSurvey())
         .expect(403)
+    })
+
+    it('Should return 204 on survey if role is admin', async () => {
+      const { accessToken } = await makeUserAccountWithRole('admin')
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(makeFakeSurvey())
+        .expect(204)
     })
   })
 })
