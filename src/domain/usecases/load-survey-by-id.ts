cd1bb4789ae442d9e1dd13e27166a47b11cc304c
albertoflorence <alberto.sabatier@gmail.com>
2022-02-14 00:08:09 -0300

    feat: add LoadSurveyById usecase

---
+++ src/domain/usecases/load-survey-by-id.ts
@@ -0,0 +1,9 @@
+import { SurveyModel } from '@/domain/models'
+
+export interface LoadSurveyById {
+  loadById: (id: string) => LoadSurveyById.Result
+}
+
+export namespace LoadSurveyById {
+  export type Result = Promise<SurveyModel>
+}
