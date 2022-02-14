f89b1c0b25e49d30a4eec0ee69e2e098bbe3f975
albertoflorence <alberto.sabatier@gmail.com>
2022-02-14 17:45:47 -0300

    feat: ensure SurveyResultMongoRepository update if survey result already exist

--- src/infra/db/mongodb/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result-mongo-repository.ts
@@ -6,13 +6,17 @@ export class SurveyResultMongoRepository implements AddSurveyResultRepository {
   async add(
     data: AddSurveyResultRepository.Params
   ): AddSurveyResultRepository.Result {
-    const document = await this.collection.insertOne(data)
-    const result = await this.collection.findOne({ _id: document.insertedId })
-    return result && this.map(result)
+    const { surveyId, accountId, answer, date, question } = data
+    const result = await this.collection.findOneAndUpdate(
+      { surveyId, accountId },
+      { $set: { answer, date, question } },
+      { upsert: true, returnDocument: 'after' }
+    )
+    return result && this.map(result.value)
   }
 
   private get collection(): Collection {
-    return MongoDbHelper.getCollection('surveys')
+    return MongoDbHelper.getCollection('surveyResults')
   }
 
   private map(data: any): any {
