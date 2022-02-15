be5f67e332e478d170ad7740a2b82f36148ed2b4
albertoflorence <alberto.sabatier@gmail.com>
2022-02-15 00:38:16 -0300

    test: ensure add survey result route return 200 on add success

--- tests/infra/db/mocks/account-mongo-mock.ts
+++ tests/infra/db/mocks/account-mongo-mock.ts
@@ -1,6 +1,9 @@
 import { mockAddAccountParams } from '@/tests/domain/mocks'
 import { AccountModel } from '@/domain/models'
 import { MongoDbHelper } from '@/infra/db'
+import { ObjectId } from 'mongodb'
+import jwt from 'jsonwebtoken'
+import { env } from '@/main/config'
 
 export const mockMongoAccount = async (): Promise<AccountModel> => {
   const collection = MongoDbHelper.getCollection('accounts')
@@ -8,4 +11,26 @@ export const mockMongoAccount = async (): Promise<AccountModel> => {
   const document = await collection.insertOne(mockAddAccountParams())
   const account = await collection.findOne({ _id: document.insertedId })
   return MongoDbHelper.map(account)
+}
+
+export const mockMongoAccountWithAccessToken =
+  async (): Promise<AccountModel> => {
+    const collection = MongoDbHelper.getCollection('accounts')
+
+    const account = await mockMongoAccount()
+    const accessToken = generateUserToken(account.id)
+    await collection.updateOne(
+      { _id: new ObjectId(account.id) },
+      { $set: { accessToken } }
+    )
+
+    return {
+      ...account,
+      accessToken
+    }
+  }
+
+const generateUserToken = (id: string): string => {
+  const accessToken = jwt.sign(id, env.jwtSecret)
+  return accessToken
 }
--- tests/main/routes/survey-result-routes.test.ts
+++ tests/main/routes/survey-result-routes.test.ts
@@ -1,10 +1,49 @@
 import { app } from '@/main/config'
 import request from 'supertest'
+import { MongoDbHelper } from '@/infra/db'
+import { Collection } from 'mongodb'
+import { mockMongoAccountWithAccessToken } from '@/tests/infra/db'
+import { mockAddSurveyParams } from '@/tests/domain/mocks'
+
+let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Result Routes', () => {
+  beforeAll(async () => {
+    await MongoDbHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoDbHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = MongoDbHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+
+    accountCollection = MongoDbHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   describe('PUT /surveys/:surveyId/results', () => {
     it('Should return 403 on add survey result without accessToken', async () => {
       await request(app).put('/api/surveys/any_id/results').send().expect(403)
+    })
+
+    it('Should return 200 on add survey result success', async () => {
+      const { accessToken } = await mockMongoAccountWithAccessToken()
+      const survey = mockAddSurveyParams()
+      const id = (
+        await surveyCollection.insertOne(survey)
+      ).insertedId.toHexString()
+
+      await request(app)
+        .put(`/api/surveys/${id}/results`)
+        .set('x-access-token', accessToken as string)
+        .send({
+          answer: survey.answers[0].answer
+        })
+        .expect(200)
     })
   })
 })
