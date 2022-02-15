624249779fd6868da7a34d7ca594c1676b122337
albertoflorence <alberto.sabatier@gmail.com>
2022-02-14 21:19:56 -0300

    feat: ensure AddSurveyResultController retrn 500 if LoadSurveyById throws

--- src/presentation/controllers/add-survey-result-controller.ts
+++ src/presentation/controllers/add-survey-result-controller.ts
@@ -1,14 +1,18 @@
 import { LoadSurveyById } from '@/domain/usecases'
-import { forbidden, ok } from '@/presentation/helpers'
+import { forbidden, ok, serverError } from '@/presentation/helpers'
 import { InvalidParamError } from '@/presentation/errors'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class AddSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
   async handler(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { surveyId } = httpRequest.params
-    const survey = await this.loadSurveyById.loadById(surveyId)
-    if (survey === null) return forbidden(new InvalidParamError('surveyId'))
-    return ok({})
+    try {
+      const { surveyId } = httpRequest.params
+      const survey = await this.loadSurveyById.loadById(surveyId)
+      if (survey === null) return forbidden(new InvalidParamError('surveyId'))
+      return ok({})
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
