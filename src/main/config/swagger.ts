50b989b4d3108ecad88b4df46f42ac35d06a3a29
albertoflorence <alberto.sabatier@gmail.com>
2022-02-15 15:21:40 -0300

    docs: add login api docs

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -2,7 +2,8 @@ import swaggerConfig from '@/main/docs'
 
 import { serve, setup } from 'swagger-ui-express'
 import { Express } from 'express'
+import { noCache } from '../middlewares/noCache'
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup(swaggerConfig))
+  app.use('/api-docs', noCache, serve, setup(swaggerConfig))
 }
