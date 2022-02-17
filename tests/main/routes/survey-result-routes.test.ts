bd3f8ed2a20cbd501b238b5dd6c24210cb56ae4f
albertoflorence <alberto.sabatier@gmail.com>
2022-02-17 18:28:12 -0300

    test: ensure survey result get route return 403 if no accessToken is provided

--- tests/main/routes/survey-result-routes.test.ts
+++ tests/main/routes/survey-result-routes.test.ts
@@ -46,4 +46,10 @@ describe('Survey Result Routes', () => {
         .expect(200)
     })
   })
+
+  describe('GET /surveys/:surveyId/results', () => {
+    it('Should return 403 on load survey result without accessToken', async () => {
+      await request(app).get('/api/surveys/any_id/results').expect(403)
+    })
+  })
 })
