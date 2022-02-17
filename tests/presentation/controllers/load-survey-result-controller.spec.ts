d8e3d4a88de66736d61c537b3dfbeb25b5194562
albertoflorence <alberto.sabatier@gmail.com>
2022-02-17 17:10:05 -0300

    test: ensure LoadSurveyResultController return 403 if LoadSurveyById returns null

--- tests/presentation/controllers/load-survey-result-controller.spec.ts
+++ tests/presentation/controllers/load-survey-result-controller.spec.ts
@@ -1,6 +1,8 @@
 import { LoadSurveyByIdStub } from '@/tests/presentation/mocks'
 import { LoadSurveyResultController } from '@/presentation/controllers'
 import { HttpRequest } from '@/presentation/protocols'
+import { forbidden } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
 
 interface SutTypes {
   sut: LoadSurveyResultController
@@ -25,5 +27,12 @@ describe('Load Survey Result Controller', () => {
     const { sut, loadSurveyByIdStub } = makeSut()
     await sut.handler(mockRequest())
     expect(loadSurveyByIdStub.id).toBe('any_survey_id')
+  })
+
+  it('Should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    loadSurveyByIdStub.result = null
+    const httpResponse = await sut.handler(mockRequest())
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 })
