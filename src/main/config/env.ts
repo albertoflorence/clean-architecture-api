7c11b44108c100cda09df8ba2b9b9846d5ffc5a2
albertoflorence <alberto@alberto.localdomain>
2022-05-06 15:48:45 -0300

    refactor: set default env variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 
 export const env = {
   mongoUrl: process.env.MONGO_URL as string,
-  port: process.env.PORT as string,
-  jwtSecret: process.env.JWT_SECRET as string,
-  bcryptSalt: parseInt(process.env.BCRYPT_SALT as string)
+  port: process.env.PORT ?? 8080,
+  jwtSecret: process.env.JWT_SECRET ?? 'SECRET123',
+  bcryptSalt: parseInt(process.env.BCRYPT_SALT ?? '12')
 }
